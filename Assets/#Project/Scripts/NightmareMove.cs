ac20c7799efa9af4cfb28f884911fe54a0b011b7
Samencre <samfievez@gmail.com>
2025-10-18 21:48:55 +0200

    walls...

--- Assets/#Project/Scripts/NightmareMove.cs
+++ Assets/#Project/Scripts/NightmareMove.cs
@@ -5,30 +5,26 @@ public class NightmareMove : MonoBehaviour
     public Transform player;         
     public float chase = 4f;    
     public float stop = 5f;     
-    public float speed = 2f;     
+    public float speed = 2f;  
+    public float space = 1f;
+    public Rigidbody2D rb;    
 
     private bool isChasing = false;  
 
-    void Update()
+void FixedUpdate()
+{
+    Debug.Log("Cible du Nightmare: " + player.name);
+    if (player == null) return;
+
+    float distance = Vector2.Distance(transform.position, player.position) + space;
+
+    if (!isChasing && distance < chase) isChasing = true;
+    else if (isChasing && distance > stop) isChasing = false;
+
+    if (isChasing)
     {
-        if (player == null) return;
-
-        // Calcul la distance entre le Nightmare et le player
-        float distance = Vector2.Distance(transform.position, player.position);
-
-        if (!isChasing && distance < chase)
-        {
-            isChasing = true; 
-        }
-        else if (isChasing && distance > stop)
-        {
-            isChasing = false; 
-        }
-
-        if (isChasing)
-        {
-            Vector2 direction = (player.position - transform.position).normalized;
-            transform.position += (Vector3)(direction * speed * Time.deltaTime);
-        }
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
     }
+}
 }
